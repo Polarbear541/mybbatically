65f3b01ecd55506574c94fec120f5b6ead2a968c
David <david@vernier.me>
2012-10-30 20:43:03 +0000

    Creates language file

---
+++ inc/languages/english/admin/mybbatically.lang.php
@@ -0,0 +1,7 @@
+<?php
+$l['mybbatically'] = 'MyBBatically';
+$l['mybbatically_desc'] = 'Automatically update your board to the latest released MyBB Version';
+$l['settings_desc'] = 'Configuration settings for the MyBBatically plugin';
+$l['enable'] = 'Enable MyBBatically?';
+$l['enable_desc'] = 'If you set this option to on, MyBBatically will function on your board.';
+?>
