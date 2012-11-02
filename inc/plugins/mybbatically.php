f8ebb0a166f4ec0ffec2a7af495e6e0e18438088
David <david@vernier.me>
2012-11-02 22:25:03 +0000

    Now uses fetch_remote_file(), therefore doesn't depend on cURL.

--- inc/plugins/mybbatically.php
+++ inc/plugins/mybbatically.php
@@ -68,13 +68,11 @@ function mybbatically_deactivate()
 
 function mybbatically_admin_tools_menu(&$sub_menu)
 {
-	global $mybb, $lang;
+	global $mybb;
 	
-	$lang->load('mybbatically');
-
 	if ($mybb->settings['mybbatically_global_switch'] == 1)
 	{
-		$sub_menu[] = array('id' => 'mybbatically', 'title' => $lang->mybbatically, 'link' => 'index.php?module=tools-mybbatically');
+		$sub_menu[] = array('id' => 'mybbatically', 'title' => 'MyBBatically', 'link' => 'index.php?module=tools-mybbatically');
 	}
 }
 
@@ -85,7 +83,7 @@ function mybbatically_admin_tools_action_handler(&$actions)
 
 function mybbatically_admin_tools_permissions(&$admin_permissions)
 {
-	$admin_permissions['mybbatically'] = $lang->allowed_to_upgrade_board;
+	$admin_permissions['mybbatically'] = 'Allowed to update board?';
 } 
 
 function recursive_move($dirsource, $dirdest)
@@ -141,9 +139,7 @@ function rmdir_recursive($dir)
 
 function mybbatically_run()
 {
-	global $config, $lang, $mybb;
-
-	$lang->load('mybbatically')
+	global $config, $mybb;
 
 	require_once MYBB_ROOT."inc/class_xml.php";
 	$contents = fetch_remote_file("http://www.mybb.com/version_check.php");
@@ -154,46 +150,26 @@ function mybbatically_run()
 	$download_url = "http://cloud.github.com/downloads/mybb/mybb16/mybb_$latest_code.zip";  
 	$file_zipped = "mybbatically.zip";
 	$file_unzipped = "mybbatically";
-	
-	$ch = curl_init();  
-	$fp = fopen("$file_zipped", "w");
-	
-	curl_setopt($ch, CURLOPT_URL,$download_url);  
-	curl_setopt($ch, CURLOPT_FAILONERROR, true);  
-	curl_setopt($ch, CURLOPT_HEADER,0);    
-	curl_setopt($ch, CURLOPT_AUTOREFERER, true);  
-	curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);  
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);  
-	curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
-	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);   
-	curl_setopt($ch, CURLOPT_FILE, $fp);
-	$page = curl_exec($ch);
-	
-	if (!$page) 
-	{  
-		echo $lang->curl_error_number.curl_errno($ch);  
-		echo $lang->curl_error. curl_error($ch);  
-		exit;  
-	}  
-
-	curl_close($ch);  
-
+	$fetch_file = fetch_remote_file($download_url);
+	$fp = fopen($file_zipped, "w");
+        fwrite($fp,$fetch_file); 
+        fclose($fp);
 
 	//Unzip the file  
 	$zip = new ZipArchive;
 
 	if(!$zip) 
 	{  
-		echo $lang->could_not_create_zip;
+		echo "<br>Could not create .zip archive";
 		exit;  
 	}  
 	if($zip->open("$file_zipped") != "true") 
 	{  
-		echo $lang->could_not_open_zip;  
+		echo "<br>Could not open $file_zipped";  
 	}  
 	if(!$zip->extractTo("$file_unzipped"))
 	{
-		echo $lang->could_not_extract_zip;
+		echo "<br>Could not extract $file_zipped";
 	}
 
 
@@ -214,7 +190,7 @@ function mybbatically_run()
 	//Remove zip
 	unlink('mybbatically.zip');
 
-	log_admin_action(array('do' => $lang->upgraded_board_on.date($mybb->settings['dateformat']).$lang->at.date($mybb->settings['timeformat'])));
+	log_admin_action(array('do' => 'Upgraded board to the latest available version on '.date($mybb->settings['dateformat']).' at '.date($mybb->settings['timeformat'])));
 }
 
 ?>
